f4e38dab60fb6489e230856fadbbb3123cf72a26
Kevin Statham <kevinpcstatham@gmail.com>
2021-02-10 13:48:48 -0500

    Learned using dictionaries.

--- dictionary/using_dictionaries.py
+++ dictionary/using_dictionaries.py
@@ -71,3 +71,40 @@ spread["future"] = tarot.pop(10)
 
 for time, card in spread.items():
   print("Your " + time + " is the " + str(card) + " card.")
+
+#Project Scrabble:
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+
+letter_to_points = {key:value for key, value in zip(letters, points)}
+letter_to_points[" "] = 0
+
+letters += [letter.lower() for letter in letters]
+points *= 2
+
+print(letter_to_points)
+
+def score_word(word):
+  point_total = 0
+  for letter in word:
+    point_total += letter_to_points.get(letter, 0)
+  return(point_total)
+
+brownie_points = score_word("BROWNIE")
+print(brownie_points)
+
+player_to_words = {"player1": ["BLUE", "TENNIS", "EXIT"], "wordNerd": ["EARTH", "EYES", "MACHINE"], "Lexi Con": ["ERASER", "BELLY", "HUSKY"], "Prof Reader": ["ZAP", "COMA", "PERIOD"]}
+
+player_to_points = {}
+def update_point_totals(player, words):
+  for player, words in player_to_words.items():
+    player_points = 0
+    for play in words:
+      player_points += score_word(play)
+    player_to_points[player] = player_points
+
+update_point_totals("player1", "HATCHET")
+print(player_to_points)
+
+def play_word(player, word):
+  player_to_words[player].append(word)
